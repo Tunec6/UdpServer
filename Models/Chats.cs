97e083ad7509554cf4746be72bb146f12cafbca9
nnrix <ngol223@mail.ru>
2024-04-16 14:01:39 +0500

    mongo

---
+++ Models/Chats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpServer.Models
+{
+    internal class Chats
+    {
+        public int ip { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ MongoConnector.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpServer
+{
+    internal class MongoConnector
+    {
+        private static MongoClient client = new MongoClient("mongodb://localhost:27017");
+        private static IMongoDatabase chatDataBase = client.GetDatabase("ChatDB");
+        private static IMongoCollection<BsonDocument> chats = chatDataBase.GetCollection<BsonDocument>("Chats");
+
+        public MongoConnector()
+        {
+
+        }
+    }
+}
