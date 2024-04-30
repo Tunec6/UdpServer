af7378e45bbc1b92df34a7fa3e76794904d2abba
Tunec <112854325+Tunec6@users.noreply.github.com>
2024-04-30 13:48:21 +0500

    Update Server.cs

--- Server.cs
+++ Server.cs
@@ -1,23 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-using UdpServer.Models;
+using UDPServer;
+using UDPServer.Models;
 
-namespace UdpServer
+namespace UDPserver
 {
     public class Server
     {
+
         public static async Task AcceptMessage()
         {
             IPEndPoint ep = new IPEndPoint(IPAddress.Any, 0);
-            using UdpClient udpClient = new UdpClient(35488);
-            Console.WriteLine("Сервер запущен");
+            UdpClient udpClient = new UdpClient(35488);
 
-            while(true)
+            Console.WriteLine("Сервер запущен");
+            while (true)
             {
                 byte[] buffer = udpClient.Receive(ref ep);
                 string data = Encoding.UTF8.GetString(buffer);
@@ -25,25 +27,26 @@ namespace UdpServer
                 Message message = Message.FromJSON(data);
                 if (message.Command == "Send")
                 {
-                    string IpFromMod = ModIP(message.IpFrom);
-                    string IpToMod = ModIP(message.IpTo);
-                    string fileName = GenerateFileName(IpFromMod, IpToMod);
+                    string ipFromMod = ModIP(message.IpFrom);
+                    string ipToMod = ModIP(message.IpTo);
+
+                    string fileName = GenerateFileName(ipFromMod, ipToMod);
                     if (fileName != null)
                     {
                         var curDir = Directory.GetCurrentDirectory();
-                        curDir =  curDir.Replace("\\", "/");
+                        curDir = curDir.Replace("\\","/");
                         string newFileName = String.Format($"{curDir}/history/{fileName}.txt");
                         Chat chat = new Chat(fileName);
                         chat.Messages.Add(message);
-                        MongoConnector.addChat(chat);
-                        
-
-                        //using var sw = new StreamWriter(newFileName, true);
+                        MongoConector.UpdateData(chat, message);
+                        // using var sw = new StreamWriter(newFileName, true);
+                        //sw.Write(ipFromMod + ":" + message.Text + "\n");
                         //await sw.WriteAsync(message.ToJSON() + "\n");
                         //sw.Flush();
                     }
                     
-                    Console.WriteLine(message.ToJSON());
+                   Console.WriteLine(message.ToJSON()); 
+                    
                 }
                 else if (message.Command == "Update")
                 {
@@ -61,62 +64,55 @@ namespace UdpServer
 
                         udpClient.Send(messageBytes, new IPEndPoint(IPAddress.Parse(message.IpFrom), 34285));
                     }
-                    catch (Exception e)
+                    catch(Exception e)
                     {
 
                     }
-
+                    
                 }
             }
-        }
 
+        }
         public static string GenerateFileName(string firstIp, string secondIp)
         {
-
-             
-           
-            string[] arr1 = firstIp.Split('_');
-            string[] arr2 = secondIp.Split('_');
-            for (int i = 0; i < 4; i++)
-            {
-                if (int.Parse(arr1[i]) > int.Parse(arr2[i]))
+            
+                string[] arr1 = firstIp.Split('_');
+                string[] arr2 = secondIp.Split('_');
+                for (int i = 0; i < 4; i++)
                 {
-                    return secondIp + "=" + firstIp;
-
+                    if (int.Parse(arr1[i]) > int.Parse(arr2[i]))
+                    {
+                        return secondIp + "=" + firstIp;
+                    }
+                    else if (int.Parse(arr1[i]) < int.Parse(arr2[i]))
+                    {
+                        return firstIp + '=' + secondIp;
+                    }
                 }
-                else if (int.Parse(arr1[i]) < int.Parse(arr2[i]))
-                {
-                    return firstIp + "=" + secondIp;
-
-                }
-
-            }
             return null;
 
-                
+
         }
-
-
+        //192.168.220.223
         public static string ModIP(string adres)
         {
             adres = adres.Replace(".", "_");
             string modIp = "";
+
             for (int i = 0; i < adres.Length; i++)
             {
                 if (adres[i] == ':')
                 {
                     modIp = adres.Substring(0, i);
                 }
-
             }
             if (modIp == "")
             {
                 modIp = adres;
             }
             return modIp;
+     
+
         }
-      
     }
 }
-
-
